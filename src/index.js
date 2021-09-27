00905e2abff7ff338e4708f4d68998747a4d65bc
Gene <33785659+Geneonosis@users.noreply.github.com>
2021-09-27 02:18:30 +0000

    trying to make page elements load dynamically

--- src/index.js
+++ src/index.js
@@ -4,39 +4,57 @@ import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import './index.css';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
+class App extends React.Component {
 
-let books = [];
-let book = '';
+    books = ['test','example','books'];
 
-const handleChange = (event) => {
-    book = event.target.value;
-}
+    constructor(props){
+        super(props);
+        this.state = {
+            book: ''
+        };
+        
+    }
 
-function addBook() {
-    console.log('adding book...');
-    books.push(book);
-    console.log(books)
-}
+    componentDidMount() {
+        this.setState({});
+    } 
 
-const title = 'Book Club'
-const addBookLabel = 'add a book'
-const headerElement = (
-    <div>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet"></link>
-        <h1>{title}</h1>
-        <div id="add-book">
-            <TextField fullWidth label="Book" variant="outlined" onChange={handleChange}/>
-            <Button id="btn" onClick={addBook} variant="contained">{addBookLabel}</Button>
+    handleChange = (event) => {
+       this.state.book = event.target.value;
+    }
+    
+    addBook() {
+        console.log('adding book...');
+        this.books.push(this.state.book);
+        console.log(this.books);
+        this.setState({reload: true})
+    }
+    
+    title = 'Book Club'
+    addBookLabel = 'add a book'
+    headerElement = (
+        <div>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+            <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet"></link>
+            <h1>{this.title}</h1>
+            <div id="add-book">
+                <TextField fullWidth label="Book" variant="outlined" onChange={this.handleChange}/>
+                <Button id="btn" onClick={this.addBook} variant="contained">{this.addBookLabel}</Button>
+            </div>
+            <div id="list-books">{this.books.map(liBook => <div>{liBook}</div>)}</div>
         </div>
-    </div>
-);
+    );
+
+    render() {
+        return this.headerElement;
+    }
+}
+
+
 
 ReactDOM.render(
-    headerElement,
+    <App/>,
     document.getElementById('root')
 );
