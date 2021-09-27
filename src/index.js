a2e6cd0658c1e9622b0089693c37d365f62217e7
Gene <33785659+Geneonosis@users.noreply.github.com>
2021-09-27 01:44:18 +0000

    designed website, added book field and button to add the book to a list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+let books = [];
+let book = '';
+
+const user = {
+    firstName: 'Gene',
+    lastName: 'Tigner'
+};
+
+export default function AddingBookField(){
+    const [value, setValue] = React.useState('Controlled');
+}
+
+const handleChange = (event) => {
+    book = event.target.value;
+}
+
+function addBook() {
+    console.log('adding book...');
+    books.push(book);
+    console.log(books)
+}
+
+const title = 'Book Club'
+const addBookLabel = 'add a book'
+const headerElement = (
+    <div>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet"></link>
+        <h1>{title}</h1>
+        <div id="add-book">
+            <TextField fullWidth label="Book" variant="outlined" onChange={handleChange}/>
+            <Button id="btn" onClick={addBook} variant="contained">{addBookLabel}</Button>
+        </div>
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    headerElement,
+    document.getElementById('root')
+);
